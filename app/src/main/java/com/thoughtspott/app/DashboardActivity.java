dfa005ffa63d61d3843f4c0665e08eb626cc3720
Ana Moreno <ana.moreno@ttu.edu>
2020-10-27 23:09:35 -0500

    logout button update

--- app/src/main/java/com/thoughtspott/app/DashboardActivity.java
+++ app/src/main/java/com/thoughtspott/app/DashboardActivity.java
@@ -6,16 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends Activity {
-    private Button logout,profile, message, map, calendar,addevent;
+    private Button profile, message, map, calendar,addevent;
+    private ImageButton logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
-        logout = this.findViewById(R.id.imageButton);
+        logout = findViewById(R.id.imageButton);
         logout.setOnClickListener(new OnClickListener(){
         @Override
             public void onClick(View v){
