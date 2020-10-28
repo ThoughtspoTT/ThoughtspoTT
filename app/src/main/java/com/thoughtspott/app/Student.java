8cfdb049d086ec312b718d6316059bceb3a167e0
mattyp1007 <71899532+mattyp1007@users.noreply.github.com>
2020-10-27 22:38:52 -0500

    now able to write array to db

--- app/src/main/java/com/thoughtspott/app/Student.java
+++ app/src/main/java/com/thoughtspott/app/Student.java
@@ -9,13 +9,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Student {
     String authID;
     String email;
-    String[] courses = {"course0", "course1", "course2", "course3", "course4", "course5"};
+    String courses[] = new String[]{"course0", "course1", "course2", "course3", "course4", "course5"};
     
 
     public Student(String studentEmail){
@@ -23,11 +24,12 @@ public class Student {
     }
 
     public void writeToDB(){
-        HashMap<String, String> student = new HashMap<>();     // create HashMap for easy db writing
+        HashMap<String, Object> student = new HashMap<>();     // create HashMap for easy db writing
+        List clist = Arrays.asList(courses);
         // put all info in map
         student.put("Email", email);
-        for(int i = 0; i < 6 && (!courses[i].equals(null) && !courses[i].equals("")); i++)
-            student.put("Courses", courses[i]);
+        //for(int i = 0; i < 6 && !courses[i].equals(""); i++)
+        student.put("Courses", clist);
 
         // write the map to Students collection in the Firestore db:
         FirebaseFirestore db = FirebaseFirestore.getInstance(); // new instance of db
