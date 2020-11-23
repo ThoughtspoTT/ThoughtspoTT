fd99386331ebbf2bf426ac343813585d214c14eb
lmilam <31598791+lmilam@users.noreply.github.com>
2020-11-23 16:18:55 -0600

    Toasts

--- app/src/main/java/com/thoughtspott/app/MainActivity.java
+++ app/src/main/java/com/thoughtspott/app/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, EnterClasses.class);
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
                 finish();
             }
