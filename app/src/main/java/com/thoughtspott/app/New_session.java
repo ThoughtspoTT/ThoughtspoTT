228842abbc68f8adfa982e13930ce206987d7460
Ana Moreno <ana.moreno@ttu.edu>
2020-11-30 18:37:18 -0600

    add_session variables

--- app/src/main/java/com/thoughtspott/app/New_session.java
+++ app/src/main/java/com/thoughtspott/app/New_session.java
@@ -1,12 +1,17 @@
 package com.thoughtspott.app;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,8 +24,10 @@ import java.util.ArrayList;
 
 public class New_session extends MainActivity {
 
-    private EditText first_name, last_name, major, biography;
-    private Button submit;
+    private TextView descriptiontextview;
+    private Button create_sessionbutton;
+    private Spinner course_spinner;
+    private TimePicker picker;
 
 
     @Override
@@ -28,12 +35,39 @@ public class New_session extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_session);
 
-       // first_name = findViewById(R.id.editTextFirstName);
-        //last_name = findViewById(R.id.editTextLastName);
-        //major = findViewById(R.id.editTextMajor);
-        //biography = findViewById(R.id.editTextBiography);
-        //submit = findViewById(R.id.buttonSubmitInfo);
+       descriptiontextview = findViewById(R.id.descriptiontextview);
+       course_spinner = findViewById(R.id.spinner4);
+       create_sessionbutton = findViewById(R.id.createsessionbutton);
+       picker =findViewById(R.id.datePicker1);
 
+
+
+        create_sessionbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour, minute;
+                String am_pm;
+                if (Build.VERSION.SDK_INT >= 23 ){
+                    hour = picker.getHour();
+                    minute = picker.getMinute();
+                }
+                else{
+                    hour = picker.getCurrentHour();
+                    minute = picker.getCurrentMinute();
+                }
+                if(hour > 12) {
+                    am_pm = "PM";
+                    hour = hour - 12;
+                }
+                else
+                {
+                    am_pm="AM";
+                }
+
+                Toast.makeText(New_session.this,"Time set to " + hour + ":" + minute + " "+ am_pm,Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
 }
