de1f7c7eeb4a1f5c04543b510fb1a9b15bd8e16a
lmilam <31598791+lmilam@users.noreply.github.com>
2020-12-01 10:25:55 -0600

    Database shit

--- app/src/main/java/com/thoughtspott/app/DatabaseClassAdd.java
+++ app/src/main/java/com/thoughtspott/app/DatabaseClassAdd.java
@@ -150,47 +150,43 @@ public class DatabaseClassAdd extends AppCompatActivity {
                             names.add(prefixName);
                         }
                     }
+                    for (int i = 0; i<prefix_array.size(); i++) {
+                        int finalI = i;
+                        (rootNode.getReference(prefix_array.get(i))).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot1) {
+                                names2 = new ArrayList<>();
+                                for (DataSnapshot chilSnap : snapshot1.getChildren()) {
+                                    String spinnerName = chilSnap.child("name") != null ? chilSnap.child("name").getValue(String.class) : null;
+                                    if (spinnerName != null) {
+                                        names2.add(spinnerName);
+                                    }
+                                }
+                                    if ((names.contains(prefix_array.get(finalI))) && (names2.contains(number_array.get(finalI)))) {
+                                        rootNode.getReference(prefix_array.get(finalI) + " " + number_array.get(finalI)).push().child("student").setValue(email);
+                                    } else if (((names.contains(prefix_array.get(finalI)))) && (!(names2.contains(number_array.get(finalI))))) {
+                                        rootNode.getReference(prefix_array.get(finalI)).push().child("name").setValue(number_array.get(finalI));
+                                        rootNode.getReference(prefix_array.get(finalI) + " " + number_array.get(finalI)).push().child("student").setValue(email);
+                                    } else {
+                                        rootNode.getReference(prefix_array.get(finalI)).push().child("name").setValue(number_array.get(finalI));
+                                        rootNode.getReference(prefix_array.get(finalI) + " " + number_array.get(finalI)).push().child("student").setValue(email);
+                                        reference.push().child("name").setValue(prefix_array.get(finalI));
+                                    }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
                 }
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
                 }
             });
-
-
-            for (int i = 0; i<prefix_array.size(); i++) {
-
-                (rootNode.getReference(prefix_array.get(i))).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot1) {
-                        names2 = new ArrayList<>();
-                        for (DataSnapshot chilSnap : snapshot1.getChildren()) {
-                            String spinnerName = chilSnap.child("name") != null ? chilSnap.child("name").getValue(String.class) : null;
-                            if (spinnerName != null) {
-                                names2.add(spinnerName);
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-                for (int j = 0; j < prefix_array.size(); j++) {
-                    if ((names.contains(prefix_array.get(j))) && (names2.contains(number_array.get(j)))) {
-                        rootNode.getReference(prefix_array.get(j) + " " + number_array.get(j)).push().child("student").setValue(email);
-                    } else if (((names.contains(prefix_array.get(j)))) && (!(names2.contains(number_array.get(j))))) {
-                        rootNode.getReference(prefix_array.get(j)).push().child("name").setValue(number_array.get(j));
-                        rootNode.getReference(prefix_array.get(j) + " " + number_array.get(j)).push().child("student").setValue(email);
-                    } else {
-                        rootNode.getReference(prefix_array.get(j)).push().child("name").setValue(number_array.get(j));
-                        rootNode.getReference(prefix_array.get(j) + " " + number_array.get(j)).push().child("student").setValue(email);
-                        reference.push().child("name").setValue(prefix_array.get(j));
-                    }
-                }
-            }
 
         }
     }
